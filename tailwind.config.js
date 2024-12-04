06d06ed92669c0da1468e26c9e57adf643f4bacf
unknown <naufalbintangpradana@gmail.com>
2024-12-04 19:17:05 +0700

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        pertama: "#2B303A",
+        kedua: "#4A505C",
+        ketiga: "#F26430",
+        putih: "#fefefe",
+      },
+      fontFamily: {
+        Kodchasan: ["Kodchasan"],
+      },
+    },
+  },
+  plugins: [],
+};
